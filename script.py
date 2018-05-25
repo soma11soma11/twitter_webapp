5365f6de140ebf6e2ef5df83652176e6a9592a64
soma11soma11 <h2so4cos0@gmail.com>
2018-05-25 20:11:08 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,37 @@
+# coding:utf-8
+from TwitterFollowBot import TwitterBot
+my_bot = TwitterBot()
+my_bot.sync_follows()
+
+
+my_bot.auto_fav("機械学習", count=10)
+my_bot.auto_fav("人工知能", count=10)
+my_bot.auto_fav("エンジニア", count=10)
+my_bot.auto_fav("ブロックチェーン", count=10)
+my_bot.auto_fav("スマートコントラクト", count=10)
+my_bot.auto_fav("落合 陽一", count=10)
+my_bot.auto_fav("MIT media lab", count=10)
+my_bot.auto_fav("研究　留学", count=10)
+my_bot.auto_fav("tensorflow", count=10)
+my_bot.auto_fav("deeplearn.js", count=10)
+my_bot.auto_fav("NetLogo", count=10)
+my_bot.auto_fav("smart city", count=10)
+my_bot.auto_fav("connpass", count=10)
+my_bot.auto_fav("早稲田", count=10)
+my_bot.auto_fav("慶応", count=10)
+my_bot.auto_fav("東京大学", count=10)
+my_bot.auto_fav("松尾研", count=10)
+my_bot.auto_fav("微積", count=10)
+my_bot.auto_fav("線形代数", count=10)
+my_bot.auto_fav("ロンドン　留学", count=10)
+my_bot.auto_fav("大学　研究", count=10)
+my_bot.auto_fav("京大　留学", count=10)
+my_bot.auto_fav("音楽 都市", count=10)
+my_bot.auto_fav("在住", count=10)
+my_bot.auto_fav("ロンドン　音楽", count=10)
+my_bot.auto_fav("ロンドン　在住", count=10)
+my_bot.auto_fav("ロンドン　学生", count=10)
+my_bot.auto_fav("空間解析", count=10)
+
+
+
